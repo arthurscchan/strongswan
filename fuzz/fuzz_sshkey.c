8d80e85e3e2678074041549efcbb8e411dced631
Arthur Chan <arthur.chan@adalogics.com>
2026-06-01 14:11:57 +0100

    OSS-Fuzz: Add fuzzer targets ssh key parsing
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

---
+++ fuzz/fuzz_sshkey.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2026 Arthur SC Chan
+ *
+ * Copyright (C) secunet Security Networks AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <library.h>
+#include <utils/debug.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
+{
+	public_key_t *key;
+	chunk_t chunk;
+
+	dbg_default_set_level(-1);
+	library_init(NULL, "fuzz_sshkey");
+	if (!lib->plugins->load(lib->plugins, PLUGINS))
+	{
+		return 1;
+	}
+
+	chunk = chunk_create((u_char*)buf, len);
+	key = lib->creds->create(lib->creds, CRED_PUBLIC_KEY, KEY_ANY,
+							 BUILD_BLOB_SSHKEY, chunk, BUILD_END);
+	DESTROY_IF(key);
+
+	lib->plugins->unload(lib->plugins);
+	library_deinit();
+	return 0;
+}
